5f67ab840ae90dfa45c15185232eba1088e58ff7
Ashada23 <90718856+Ashada23@users.noreply.github.com>
2022-10-15 20:23:14 +0800

    Pertambahan 2 angka

--- src/Pertambahan2Angka/Interface.java
+++ src/Pertambahan2Angka/Interface.java
@@ -182,9 +182,6 @@ public class Interface extends javax.swing.JFrame {
         Double Hasil = an1 + an2;
 
         jTextField3.setText(Double.toString(Hasil));
-        
-        
-        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
